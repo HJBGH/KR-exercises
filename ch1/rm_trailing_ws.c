e665424c9792874aadf3d3d98ad48ce6db030ea9
HB <hb@localhost.localdomain>
2018-01-18 17:21:47 +1100

    Got a prototype for exercise 1-18 working, it doesn't handle input longer than its buffer unfortunately.

--- ch1/rm_trailing_ws.c
+++ ch1/rm_trailing_ws.c
@@ -3,41 +3,66 @@
 
 #include <stdio.h>
 #define MAX_LENGTH 128
-int remove_trailing_ws(char line[], int ll); //ll -> line length
+int remove_trailing_ws(char line[]); /*line[] should be terminated with a null termi or a 
+										newline*/
+int getline(char line[], int ll); /*ll > line length*/
 
-main()
-{
 
+int main()
+{	
+	char line[MAX_LENGTH];
+	int len, newlen;
+	while((len = getline(line, MAX_LENGTH)) > 0)
+	{
+		newlen = remove_trailing_ws(line);
+		if(newlen > 0)
+		{
+			printf("%s", line);
+		}
+	}
+	return 0;
 }
 
-/*this function gets a newline and processes it in one go, perhaps I should
- * separate these two operations*/
-int remove_trailing_ws(char line[], int ll)
+int getline(char line[], int ll)
 {
-	int i, c, k; /*you should know what i and c are by now, k is used to record
-	the position of the last non whitespace, non newline character*/
+	int c, i;
 	for(i = 0; i < ll-1 && (c = getchar()) != EOF && c != '\n'; i++)
 	{
 		line[i] = c;
-		if(c != ' ' && c != '\t')
+	}
+	if(c == '\n')
+	{
+		line[i] = c;
+		i++;
+	}
+	line[i] = '\0';
+	return i;
+}
+
+	
+int remove_trailing_ws(char line[])
+{
+	int i, k; /*you should know what i and c are by now, k is used to record
+	the position of the last non whitespace, non newline character*/
+	k = 0;
+	for(i = 0; line[i] != EOF && line[i] != '\n'; i++)
+	{
+		if(line[i] != ' ' && line[i] != '\t')
 		{
 			k = i;
 		}
 	}
-
 	/*doesn't handle full buffer array nicely, crashes*/
-	if(c == '\n')
+	if(line[i] == '\n' && k > 0)
 	{
 		k++;
 		line[k] == '\n';
-		k++
+		k++;
 		line[k] == '\0';
 	}
-	/*delete lines that are entirely whitespace*/
-	if(k == 0)
+	else
 	{
-		return 0;
+		line[k] == '\0';	
 	}
-	return i
-	
-	/*need to consider edge cases*/
+	return k;
+}	
