393429426e06772db9bd87ab02dfdbc358198647
HB <hb@localhost.localdomain>
2018-01-22 12:39:15 +1100

    Added exercise 2-3 stub file, htoi.c

---
+++ ch2/htoi.c
@@ -0,0 +1,27 @@
+/*contains the function htoi(s), which converts the string representation 
+ *of a hexadecimal integer into it's integer counterpart. This file also 
+ contains a small test driver in main() and a utility function for converting
+ the alphabetical characters of the hex string into integer values*/
+
+#include <stdio.h> 
+
+
+int htoi(char[] hex_str);/*hex string to in converter*/
+int hatoi_util(char c); 
+/*hatoi is a utility function for determining the validity of an alphabetical
+ * character in a hex string, returns -1 if invalid char*/
+
+main()
+{
+	/*TODO; write test cases*/
+}
+
+int htoi(char[] hex_str)
+{
+	/*TODO*/
+}
+
+int hatoi_util(char c)
+{
+	/*TODO*/
+}
