a3e78689368e3743f797fc9be80956fabb5b0596
HB <hb@localhost.localdomain>
2018-01-22 16:41:34 +1100

    I'm an idiot and my brain doesn't work right. Added a sol a tested working solution to 2-4

--- ch2/new_squeeze.c
+++ ch2/new_squeeze.c
@@ -8,6 +8,14 @@ void squeeze(char s1[], char s2[]);
 int main()
 {
 	/*TODO: write test calls*/
+	char test[] = "I'm a goofy goober yea!";
+	char remove[] = "fog!";
+	squeeze(test, remove);
+	printf("%s\n", test);
+	char test2[] = "the quicAk browYn foxY jumpeLd Mover the lAzy dOg";
+	char remove2[] = "abcdefghijklmnopqrstuvwxyz";
+	squeeze(test2, remove2);
+	printf("%s\n", test2);
 }
 
 void squeeze(char s1[], char s2[])
@@ -15,9 +23,9 @@ void squeeze(char s1[], char s2[])
 	/*the O(mn) complexity isn't great.*/
 	int i, j, k;
 
-	for(i = 0; i != '\0'; i++)
+	for(i = 0; s2[i] != '\0'; i++)
 	{
-		for(j = k = 0; j!= '\0'; j++)
+		for(j = k = 0; s1[j] != '\0'; j++)
 		{
 			if(s1[j] != s2[i])
 			{
