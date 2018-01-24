36471cebc94b182738370b1bc8749ad3b035a6b9
HB <hb@localhost.localdomain>
2018-01-24 19:41:34 +1100

    added stub file for exercise 2-7 solution

---
+++ ch2/invert.c
@@ -0,0 +1,17 @@
+/*Solution to exercise 2-7, returns x with the n bits that begin at position p 
+ * inverted, leaves the others unchanged*/
+
+#include <stdio.h>
+
+unsigned invert(unsigned x, int n, int p); /* prototype*/
+
+int main()
+{
+	/*TODO: write tests*/
+	return 0;
+}
+
+unsigned invert(unsigned x, int n, int p)
+{
+	/*TODO*/
+}
