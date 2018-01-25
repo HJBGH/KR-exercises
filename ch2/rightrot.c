7a414414d6ad322b6632d70c23a66e64701205dc
HB <hb@localhost.localdomain>
2018-01-25 14:53:26 +1100

    Added stubfile for exercise 2-8 solution

---
+++ ch2/rightrot.c
@@ -0,0 +1,23 @@
+/*This is a response to exercise 2-8 from on page 49,
+ * return the value of the integer x rotated to the right by n bit positions*/
+
+#include <stdio.h>
+
+unsigned rightrot(unsigned x, int n);
+
+int main()
+{
+	/*TODO: write tests*/
+	return 0;
+}
+
+unsigned rightrot(unsigned x, int n)
+{
+	/*TODO*/
+	/*Get rightmost n bits into a mask*/
+	/*shift x n bits to the right*/
+	/*shift the mask sizeof(x)-n to the left*/
+	/* return x OR mask*/
+}
+
+
