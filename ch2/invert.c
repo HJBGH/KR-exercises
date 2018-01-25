13a3b4701b583ca959eba3443ae5e84e48bcd19e
HB <hb@localhost.localdomain>
2018-01-25 14:44:31 +1100

    My solution to 2-7 is now functioning

--- ch2/invert.c
+++ ch2/invert.c
@@ -7,14 +7,16 @@ unsigned invert(unsigned x, int n, int p); /* prototype*/
 
 int main()
 {
-	/*TODO: write tests*/
-	printf("%u -> %u\n", 403,  invert(403, 5, 3));
+	/*TODO: write better tests*/
+	printf("%u -> %u\n", 3855,  invert(3855, 7, 4));
+	printf("%u -> %u\n", 3920,  invert(3920, 7, 4));
+
 	return 0;
 }
 
 unsigned invert(unsigned x, int p, int n)
 {
-	unsigned mask = ((~x >> (p+1-n)) & ~(~0 << n));
+	unsigned mask = ((~x >> (p+1-n)) & ~(~0 << n)) << (p+1-n);
 	printf("one's complement x: %u\n", ~x);
 	printf("shifted x: %u\n", (~x >> (p+1-n)));
 	printf("mask: %u\n", mask);
