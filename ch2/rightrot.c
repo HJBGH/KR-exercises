f3f6778707c0ea74f0dfc40df4442e5fb43fa804
HB <hb@localhost.localdomain>
2018-01-25 15:50:08 +1100

    Added solution for exercise 2-8

--- ch2/rightrot.c
+++ ch2/rightrot.c
@@ -7,17 +7,21 @@ unsigned rightrot(unsigned x, int n);
 
 int main()
 {
-	/*TODO: write tests*/
+	/*TODO: write more tests*/
+	printf("%u -> %u\n", 255, rightrot(255, 4));
 	return 0;
 }
 
 unsigned rightrot(unsigned x, int n)
 {
-	/*TODO*/
 	/*Get rightmost n bits into a mask*/
 	/*shift x n bits to the right*/
 	/*shift the mask sizeof(x)-n to the left*/
 	/* return x OR mask*/
+	unsigned mask = (x & ~(~0 << n)) << ((sizeof(x)*8) - n); /*get rightmost n bits*/
+	printf("mask: %u\n", mask);
+	x = x >> n;
+	return x | mask;
 }
 
 
