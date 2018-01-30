98fd0a7d03d33737451ead9e650eacca2c95ac77
HB <HJBowland@protonmail.com>
2018-01-30 20:46:02 +1100

    As it turns out I don't have the attention to solve this problem in one sitting

--- ch1/fold.c
+++ ch1/fold.c
@@ -9,7 +9,8 @@
  * where the line has no blanks before the threshold*/
 
 /*prints the folded version of the line*/
-int fold_line(char line[]);
+/*newline should be longer than line*/
+int fold_line(char line[], char newline[]);
 int getline(char line[], int line_len);
 
 int main()
@@ -35,8 +36,37 @@ int getline(char line[], int ll)
 	return i;
 }
 
-int fold(char line[])
+/*there is no defensive programming in this function, the ideal use case is
+ * all that is considered*/
+int fold(char line[], char newline[])
 {
-	/*TODO*/
+	int last_blank = -1;
+	int i, j;
+	for(j = i = 0; line[i] != '\n' && line[i] != '\0'; i++, j++)
+	{
+		newline[j] = line[i];
+		if(line[i] == '\t' || line[i] == ' ')
+		{
+			last_blank = i;
+		}
+		if(i%THRESHOLD == 0)/*Handle really long lines*/
+		{
+			/*descisions to be made here*/
+			/*normal case found*/
+			if(last_blank > -1)
+			{
+				newline[last_blank] = '\n';
+				last_blank = -1;
+			}
+			/*things get interesting here, we have to an additonal two
+			 * characters to indicate that a line has been continued into
+			 * it's neighbour */
+			else if(last_blank == -1)
+			{
+				
+			}
+			/*another case for blank found*/
+		}
+	}
 	return 0;
 }
