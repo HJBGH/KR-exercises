24953f9c8b9e863b859743449ff52142677b42eb
HB <HJBowland@protonmail.com>
2018-02-01 19:08:19 +1100

    Added stub code to the 1-23 solution

--- ch1/comments_b_gone.c
+++ ch1/comments_b_gone.c
@@ -10,4 +10,22 @@
 /*comments may span multiple lines, which means that the input cannot be
  * interpreted on a strictly line by line basis*/
 /*this program only checks for ANSI style comments with a slash and an asterix*/
-int comments_b_gone(char line[]); /*use a sliding window to read*/
+/*unfortunately the structure of this program is going to be fairly basic
+ * in order to avoid having an expressed limit on the size of the input.*/
+
+int main()
+{
+	int c; /*character*/
+	int nextc; /*next character, together these form the sliding window*/
+
+	c = getchar();
+	if(c == EOF)
+	{
+		return; /*spaghetti defensive programming*/
+	}
+
+	while((nextc = getchar())!= EOF)
+	{
+		/*do the thing*/
+	}
+}
