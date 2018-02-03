bf01ce38d8db87ce6598b39a98113974448d936d
HB <HJBowland@protonmail.com>
2018-02-03 14:15:51 +1100

    Slightly fleshed out stub code to exercise 1-24 solution

--- ch1/ex_1_24.c
+++ ch1/ex_1_24.c
@@ -5,17 +5,42 @@
 
 #define STACK_SIZE 1024
 
-enum states {COMMENT, CODE};
+/*used to determine where the program is as it reads the source code*/
+enum states {COMMENT, CODE, STRING};
 
 int main()
 {
 	/*ideally I'd use a linked list to implement this stack, but we haven't
 	 * reached that point in the book yet and I'd like to keep things by
 	 * the numbers*/
+	/*use multiple stacks for different symbols?*/
 	char stack[STACK_SIZE];
 	int c;
 	int nextc;
 	int stack_head; /*This would be better as a pointer*/
 	enum states state = CODE;
+
+	c = getchar();
+	if(c == EOF)
+	{
+		return 0;
+	}
+
+	while((nextc = getchar()) != EOF)
+	{
+		/*analyse syntax*/
+		if(state == CODE)
+		{
+			/*handle cases*/
+		}
+		else if(state == COMMENT)
+		{
+			/*handle cases*/
+		}
+		else if(state == STRING)
+		{
+			/*handle cases*/
+		}
+	}
 }
 
