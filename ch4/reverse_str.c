6123a91d942e0c82147afea555431fc88ba6c1ba
HB <HJBowland@protonmail.com>
2018-02-14 16:03:09 +1100

    Added solution to 4-13

--- ch4/reverse_str.c
+++ ch4/reverse_str.c
@@ -3,24 +3,27 @@
 #include <stdio.h>
 #include <string.h>
 
-void swap(int x, int y); /*non generic swap routine, used in reverse*/
 void reverse(char string[], int left, int right); /*on the initial call, left is 0, right is strlen()-1;*/
 
 int main()
 {
 	/*TODO: write tests*/
+	char test[] = "Test string";
+	reverse(test, 0, strlen(test)-1);
+	printf("%s\n", test);
 	return 0;
 }
 
 void reverse(char string[], int left, int right)
 {
-	/*TODO*/
+	if(left < right)
+	{
+		reverse(string, left+1, right-1);
+	}
+	printf("left -> %d\n", left);
+	printf("right -> %d\n", right);
+	char temp = string[left];
+	string[left] = string[right];
+	string[right] = temp;
 }
 
-void swap(int x, int y)
-{
-	int temp;
-	temp = x;
-	x = y;
-	y = temp;
-}
