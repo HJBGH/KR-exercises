c168b33367e4751fe171a288c9dd1cd6f559e27a
HB <HJBowland@protonmail.com>
2018-02-15 22:28:29 +1100

    got a prototype solution to 5-4 working

--- ch5/strend.c
+++ ch5/strend.c
@@ -16,19 +16,41 @@ int main()
 int strend(char * s, char * t)
 {
 	char * t_start = t; /*save starting position, avoids indexing*/
-	while(*s++ != '\0' && *t++ != '\0');
+	printf("t_start -> %c\n", *t_start);
+	while(*s != '\0' && *t != '\0')
+	{
+		t++;
+		s++;
+	}
+
+	if(*t == '\0')
+		printf("t is where it should be\n");
 
 	if(*s == '\0' && *t != '\0')
+	{	
+		printf("t is longer than s\n");
 		return 0; /*string t is longer than string s*/
+	}
 	else
 	{
-		while(*s++ != '\0');
+		printf("incrementing s\n");
+		while(*s != '\0') s++;
 	}
+	
+	if(*t == *s)
+		printf("true\n");
 
 	while(t != t_start)
 	{
-		if(t-- != s--)
+		/*this should run more than once*/
+		printf("t -> %c and s -> %c\n", *t, *s);
+		if(t == t_start)
 		{
+			printf("FUCK");
+		}
+		if(*t-- != *s--)
+		{
+			printf("%c and %c are not the same\n", *t, *s);
 			return 0;
 		}
 	}
