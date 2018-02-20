6dc6893270340e785cde6f0029314a7c4aee1e95
HB <HJBowland@protonmail.com>
2018-02-20 15:45:09 +1100

    Added stubfile for 5-6, I don't know if I actually want to do that exercise

---
+++ ch5/five-six.c
@@ -0,0 +1,26 @@
+/*This contains pointer based re-implementations of some of the functions
+ * given in K & R second ed, as per exercise 5-6*/
+
+#include <stdio.h>
+#include <string.h>
+
+int getline(char * s, size_t n); /*get n characters int s, return it's length*/
+int getop(char * s); /*get next operator or numeric operand*/
+
+int main()
+{
+	/*TODO: write tests*/
+	return 0;
+}
+
+int getline(char * s, size_t n)
+{
+	/*TODO: write this function*/
+	return 0;
+}
+
+int getop(char * s)
+{
+	/*TODO: write this function*/
+	return 0;
+}
