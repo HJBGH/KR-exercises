0a13b6674cc4db8cfaee05cfad2fe2f63108e5d0
HB <HJBowland@protonmail.com>
2018-02-21 19:11:39 +1100

    Added a solution to 5-9

--- ch5/pointers_md.c
+++ ch5/pointers_md.c
@@ -1,6 +1,8 @@
 /*copies of the month_day and day_of_year functions from K&R chapter 5,
  * these have been edited according to exercise 5-8*/
 
+#include <stdio.h>
+
 int day_of_year(int year, int month, int day);
 void month_day(int year, int yearday, int *pmonth, int *pda);
 
@@ -12,6 +14,7 @@ static char daytab[2][13] = {
 int main()
 {
 	/*TODO; write tests to try to break the functions*/
+	printf("%d\n", day_of_year(2012, 2, 4));
 	return 0;
 }
 
@@ -24,13 +27,13 @@ int day_of_year(int year, int month, int day)
 		return -1;/*clumsy error code*/
 	}
 	leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
-	if(day > daytab[leap][month])
+	if(day > *(*(daytab + leap) + month))
 	{
 		return -1;
 	}
 	for(i = 1; i < month; i++)
 	{
-		day += daytab[leap][i];
+		day += *(*(daytab + leap)+i);
 	}
 	return day;
 }
@@ -42,17 +45,21 @@ void month_day(int year, int yearday, int * pmonth, int * pday)
 	int i, leap;
 	if(year < 1752 || yearday < 1)
 	{
-		return -1;/*Another clumsy error code*/
+		*pmonth = -1;/*Another clumsy error code*/
+		*pday = -1;
+		return;
 	}
 
 	leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
 	if((!leap && yearday > 365) || (leap && yearday > 366))
 	{
-		return -1;
+		*pmonth = -1;
+		*pday = -1;
+		return;
 	}
-	for(i = 1; yearday > daytab[leap][i]; i++)
+	for(i = 1; yearday > *(*(daytab+leap)+i); i++)
 	{
-		yearday -= daytab[leap][i];
+		yearday -= *(*(daytab+leap)+i);
 	}
 	*pmonth = i;
 	*pday = yearday;
