68fcb67b14a16eb089f820414f65cd53079439b9
HB <HJBowland@protonmail.com>
2020-09-19 13:59:26 +1000

    retabbing detab.c

--- ch1/detab.c
+++ ch1/detab.c
@@ -11,29 +11,29 @@ int detabline(char line[]); /*does detab operations until it encounters EOF or \
 
 main()
 {
-	char line[LINE_LENGTH];
-	int len;
-	while((len = getline(line, LINE_LENGTH)) > 0)
-	{
-		detabline(line);
-	}
-	return 0;
+  char line[LINE_LENGTH];
+  int len;
+  while((len = getline(line, LINE_LENGTH)) > 0)
+  {
+    detabline(line);
+  }
+  return 0;
 }
 
 int getline(char line[], int ll)
 {
-	int c, i;
-	for(i = 0; i < ll-1 && (c = getchar()) != EOF && c != '\n'; i++)
-	{
-		line[i] = c;
-	}
-	if(c == '\n')
-	{
-		line[i] = c;
-		i++;
-	}
-	line[i] = '\0';
-	return i;
+  int c, i;
+  for(i = 0; i < ll-1 && (c = getchar()) != EOF && c != '\n'; i++)
+  {
+    line[i] = c;
+  }
+  if(c == '\n')
+  {
+    line[i] = c;
+    i++;
+  }
+  line[i] = '\0';
+  return i;
 }
 
 /*An unfortunate consequence of converting all tab chars to the equivalent
@@ -41,26 +41,26 @@ int getline(char line[], int ll)
  * memory space by an uncomfortable amount*/
 int detabline(char line[])
 {
-	/*used to construct the version of the line with the tabs replaced*/
-	int i, j, k;
-	for(i = 0, k = 0;	line[i] != EOF && line[i] != '\n'; i++)
-	{
-		if(line[i] == '\t')
-		{
-			/*handle tabs*/
-			for(j = TAB_COLUMNS - (k%TAB_COLUMNS); j>0; j--, k++)
-			{
-				/*printf("%d\n", j);*/
-				putchar('#');
-			}
+  /*used to construct the version of the line with the tabs replaced*/
+  int i, j, k;
+  for(i = 0, k = 0; line[i] != EOF && line[i] != '\n'; i++)
+  {
+    if(line[i] == '\t')
+    {
+      /*handle tabs*/
+      for(j = TAB_COLUMNS - (k%TAB_COLUMNS); j>0; j--, k++)
+      {
+        /*printf("%d\n", j);*/
+        putchar('#');
+      }
 
-		}
-		else
-		{
-			putchar(line[i]);
-			k++;
-		}
-	}
-	if(line[i] == '\n')
-		putchar('\n');
+    }
+    else
+    {
+      putchar(line[i]);
+      k++;
+    }
+  }
+  if(line[i] == '\n')
+    putchar('\n');
 }
